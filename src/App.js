c82cfd3a0848f9fd59f8c32b08857464b84132ab
YashKanpariya <yashkanpariya5@gmail.com>
2023-01-13 11:20:09 +0530

    news api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import { useEffect } from 'react';
+// import Weather from './Weather';
+import News from './News';
+// import Cricapi from './Cricapi';
+import './style.css';   
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Route, Router, Routes } from 'react-router-dom';
+import Categories from './Categories';
+
+function App() {  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     {/* <Weather/> */}
+     {/* <News/> */}
+     {/* <Cricapi /> */}
+     <Routes>
+      <Route path='/' element={<News/>} />
+      <Route path='/Categories/:cate' element={<Categories/>} />
+     </Routes>
     </div>
   );
 }
